b513a9126e159b55f40878fe93a24453560b80bb
Катя <katya.dendisol@gmail.com>
2023-10-31 02:03:47 +0300

    add button "choose file"

--- SP2/SP2.cpp
+++ SP2/SP2.cpp
@@ -11,23 +11,70 @@
 
 using namespace std;
 HWND hWnd;
-int fontSize = 20;
+HWND hButton;
+int fontSize = 40;
 const int COL_NUMBER = 3;
 const int ROW_NUMBER = 3;
 const int CELL_NUMBER = COL_NUMBER * ROW_NUMBER;
 string textPieces[CELL_NUMBER];
 
+// Панель
+int panelWidth = 0;
+int panelHeight = 50;
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 void InitDC(HWND hWnd, int windowWidth, int windowHeight);
+string ConvertUTF8ToANSI(const string& utf8Str);
 
-// Функция для преобразования UTF-8 в ANSI
+void OnButtonClick(HWND hWnd)
+{
+  OPENFILENAME ofn;
+  wchar_t szFileName[MAX_PATH] = L"";
+
+  ZeroMemory(&ofn, sizeof(ofn));
+  ofn.lStructSize = sizeof(ofn);
+  ofn.lpstrFile = szFileName;
+  ofn.lpstrFile[0] = '\0';
+  ofn.hwndOwner = hWnd;
+  ofn.nMaxFile = sizeof(szFileName);
+  ofn.lpstrFilter = L"Text Files\0*.txt\0All Files\0*.*\0";
+  ofn.nFilterIndex = 1;
+
+  if (GetOpenFileName(&ofn))
+  {
+    // Создаем временный массив для новых данных
+    string newtextPieces[CELL_NUMBER];
+    memset(newtextPieces, 0, sizeof(newtextPieces));
+
+    ifstream in(szFileName);
+    string line;
+    int i = 0;
+    while (getline(in, line))
+    {
+      istringstream iss(line);
+      string word;
+      int col = 0;
+      while (iss >> word)
+      {
+        newtextPieces[i * COL_NUMBER + col] = ConvertUTF8ToANSI(word);
+        col++;
+      }
+      i++;
+    }
+
+    // Перенос новых данных в основной массив
+    memcpy(textPieces, newtextPieces, sizeof(newtextPieces));
+
+    InvalidateRect(hWnd, NULL, TRUE);
+  }
+}
+
 string ConvertUTF8ToANSI(const string& utf8Str)
 {
   int requiredSize = MultiByteToWideChar(CP_UTF8, 0, utf8Str.c_str(), -1, NULL, 0);
   if (requiredSize == 0)
   {
-    // Обработка ошибки
-    return string(); // Возвращаем пустую строку в случае ошибки
+    return string();
   }
 
   vector<wchar_t> wideBuffer(requiredSize);
@@ -36,8 +83,7 @@ string ConvertUTF8ToANSI(const string& utf8Str)
   requiredSize = WideCharToMultiByte(CP_ACP, 0, &wideBuffer[0], -1, NULL, 0, NULL, NULL);
   if (requiredSize == 0)
   {
-    // Обработка ошибки
-    return string(); // Возвращаем пустую строку в случае ошибки
+    return string();
   }
 
   vector<char> ansiBuffer(requiredSize);
@@ -59,7 +105,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLin
   wcex.hInstance = hInstance;
   wcex.hIcon = NULL;
   wcex.hCursor = LoadCursorW(NULL, IDC_ARROW);
-  HBRUSH hBkgrndBrush = CreateSolidBrush(RGB(255, 255, 255)); // White background
+  HBRUSH hBkgrndBrush = CreateSolidBrush(RGB(255, 255, 255));
   wcex.hbrBackground = hBkgrndBrush;
   wcex.lpszMenuName = NULL;
   wcex.lpszClassName = L"Lab2";
@@ -72,22 +118,6 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLin
 
   hWnd = CreateWindowEx(0, L"Lab2", L"SP2", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
     CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
-
-  ifstream in("C:/Users/Катя/Desktop/5 сем/СП/SP2/ff.txt");
-  string line;
-  int i = 0;
-  while (getline(in, line))
-  {
-    istringstream iss(line);
-    string word;
-    int col = 0;
-    while (iss >> word)
-    {
-      textPieces[i * COL_NUMBER + col] = word;
-      col++;
-    }
-    i++;
-  }
 
   ShowWindow(hWnd, nCmdShow);
   UpdateWindow(hWnd);
@@ -106,10 +136,36 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
   switch (uMsg)
   {
+  case WM_CREATE:
+  {
+    hButton = CreateWindow(L"BUTTON", L"Выбрать файл", WS_CHILD | WS_VISIBLE, 10, 10, 120, 30, hWnd, (HMENU)1001, NULL, NULL);
+    if (hButton == NULL)
+    {
+      MessageBox(hWnd, L"Создание кнопки не удалось!", L"Ошибка", MB_ICONERROR);
+    }
+    break;
+  }
+  case WM_COMMAND:
+  {
+    if (LOWORD(wParam) == 1001 && HIWORD(wParam) == BN_CLICKED)
+    {
+      OnButtonClick(hWnd);
+    }
+    break;
+  }
   case WM_SIZE:
+  {
     width = LOWORD(lParam);
     height = HIWORD(lParam);
+
+    // Обновляем размеры и положение панели
+    panelWidth = width;
+    SetWindowPos(hButton, NULL, 10, 10, 120, 30, SWP_NOZORDER);
+
+    // Пересчитываем размеры и перерисовываем таблицу
+    InvalidateRect(hWnd, NULL, TRUE);
     break;
+  }
   case WM_PAINT:
     InitDC(hWnd, width, height);
     break;
@@ -136,10 +192,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 void DrawTable(HDC currDC, int windowWidth, int windowHeight)
 {
   int columnWidth = windowWidth / COL_NUMBER;
-  int rowHeight = windowHeight / ROW_NUMBER;
-  int x, y = 0;
+  int rowHeight = (windowHeight - panelHeight) / ROW_NUMBER;
+  int x, y = panelHeight;
 
-  // Устанавливаем цвет линий (черный)
+  // Цвет линий
   HPEN hPen = CreatePen(PS_SOLID, 1, RGB(0, 0, 0));
   HPEN hOldPen = (HPEN)SelectObject(currDC, hPen);
 
@@ -153,7 +209,7 @@ void DrawTable(HDC currDC, int windowWidth, int windowHeight)
       string ansiText = ConvertUTF8ToANSI(textPieces[i * COL_NUMBER + j]);
       DrawTextA(currDC, ansiText.c_str(), -1, &cellRect, DT_EDITCONTROL | DT_WORDBREAK);
 
-      // Рисуем вертикальные линии между столбцами
+      // Вертикальные линии между столбцами
       if (j < COL_NUMBER - 1)
       {
         MoveToEx(currDC, x + columnWidth, y, NULL);
@@ -163,7 +219,7 @@ void DrawTable(HDC currDC, int windowWidth, int windowHeight)
       x += columnWidth;
     }
 
-    // Рисуем горизонтальные линии между строками
+    // Горизонтальные линии между строками
     if (i < ROW_NUMBER - 1)
     {
       MoveToEx(currDC, 0, y + rowHeight, NULL);
@@ -177,7 +233,6 @@ void DrawTable(HDC currDC, int windowWidth, int windowHeight)
   SelectObject(currDC, hOldPen);
   DeleteObject(hPen);
 }
-
 
 void InitDC(HWND hWnd, int windowWidth, int windowHeight)
 {
